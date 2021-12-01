ec48b9331fac3a00320e010a21e7bbf817817d09
natchapolk <tactiledev.nat@gmail.com>
2021-12-01 23:38:20 +0700

    Add procfile.

--- main.py
+++ main.py
@@ -6,4 +6,3 @@ app = FastAPI()
 async def getIndex():
 	return RedirectResponse("index.html")
 app.mount("/", StaticFiles(directory="web"), name="web")
-web: uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-5000}
